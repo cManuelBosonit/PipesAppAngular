2f90a11099bdf9819b3d907684c3cf3ac093e216
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-03-23 10:25:16 +0100

    Pipes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipesApp';
+export class AppComponent implements OnInit{
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  nombre: string = 'Manuel herNandez';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Manuel'
+  }
+  
+  mostrarNombre(){
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
